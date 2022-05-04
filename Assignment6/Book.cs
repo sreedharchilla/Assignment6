ebc943df4f8fda2640025502ec7f1dbabe0d0c98
sreedharchilla <103948938+sreedharchilla@users.noreply.github.com>
2022-05-04 23:04:22 +0530

    c#Assignment6

---
+++ Assignment6/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+
+
+    struct Book
+    {
+        public string Name;
+        private int Book_Id { get; set; }
+
+        public int Book_ID
+        {
+            get { return Book_Id; }
+            set { Book_Id = value; }
+        }
+        public void Display()
+        {
+            Console.WriteLine(" The Book id is:{0}", Book_Id);
+            Console.WriteLine(" The Book name is:{1}", Name);
+        }
+
+
+    }
+    class StructEg1
+    {
+        static void Main()
+        {
+            Book b = new Book();
+            b.Book_ID = 111;
+            b.Name = "Wings Of Fire";
+            b.Display();
+            Console.ReadLine();
+        }
+
+    }
+}
+    
+
+
---
+++ Assignment6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    class Program
+    {
+        interface IStudent
+        {
+            void ShowDetails(int id, string name);
+        }
+        class DaySholar : IStudent
+        {
+            public void ShowDetails(int id, string name)
+            {
+                Console.WriteLine(" The Student Id is:{0}\nName of the student is:{1}", id, name);
+            }
+        }
+        class Resident : IStudent
+        {
+            public void ShowDetails(int id, string name)
+            {
+                Console.WriteLine(" The Student id is:{0}\n Name of the Student is{1}", id, name);
+            }
+        }
+        class InterfaceEg
+        {
+            static void Main()
+            {
+                Console.WriteLine("-----Dayscholar Details-----");
+                IStudent d = new DaySholar();
+                d.ShowDetails(1, "Pavan");
+
+                Console.WriteLine("------Resident Scholar Details-----");
+                IStudent r = new Resident();
+                r.ShowDetails(2, "Kalyan");
+
+            }
+        }
+    }
+}
+    
